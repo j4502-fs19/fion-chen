69fd2390d00d8f34b032a67e61ad6d4d2663517c
fionpengchen <54375379+fionpengchen@users.noreply.github.com>
2019-10-26 10:32:05 -0500

    create a hamburger menu

---
+++ menu.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".menu-trigger").click(function(){
+    $("#nav").slideToggle(500);
+  });
+
+  $(window).resize(function(){
+    if($(window).width()>600){
+      $("#nav").removeAttr("style");
+  }
+});
+});
